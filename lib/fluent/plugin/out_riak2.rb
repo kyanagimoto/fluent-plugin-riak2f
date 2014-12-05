6e30a2fed99e449fba054d307e8a0cf63296acaf
Matt Nunogawa <@amattn>
2014-12-04 17:49:04 -0800

    refactor to support riak 2.0 bucket types and yokozuna json indexing

--- lib/fluent/plugin/out_riak2.rb
+++ lib/fluent/plugin/out_riak2.rb
@@ -8,7 +8,9 @@ class Riak2Output < BufferedOutput
   include SetTagKeyMixin
   config_set_default :include_time_key, true
 
+  config_param :bucket_type, :string, :default => "fluentlog" # we assume that search is on and this bucket type has an search index attached to it.
   config_param :bucket_name, :string, :default => "fluentlog"
+  config_param :riak2_metadata_bucket_type, :string, :default => ""
   config_param :nodes, :string, :default => "localhost:8087"
 
   def initialize
@@ -30,10 +32,31 @@ class Riak2Output < BufferedOutput
 
   def start
     $log.debug " => #{@buffer.chunk_limit} #{@buffer.queue_limit} "
-    @conn = Riak::Client.new(:nodes => @nodes)
-    @bucket = @conn.bucket(@bucket_name)
+    @client = Riak::Client.new(:nodes => @nodes)
+    @bucket = @client.bucket(@bucket_name)
     @buf = {}
 
+    # $log.debug "riak2_metadata_bucket_type => #{@riak2_metadata_bucket_type}"
+    # $log.debug "bucket_type => #{@bucket_type}"
+
+    if not @riak2_metadata_bucket_type.empty? then
+      # Here we are storing our bucket type and bucket name in a metadata map.  This allows clients to query that map to see a list of all fluentd buckets.
+
+      # bucket_type/name/key is returns a metadata map
+      metadata_bucket_type = @riak2_metadata_bucket_type        # config defined bucket type
+      metadata_bucket_name = "fluent-plugin-riak2-metadata"     # bucket name
+      metadata_key         = "fluent-plugin-riak2-metadata-key" # root level key for our metadata map
+
+      # our metadata map has a kv where:
+      # 1. key is set_of_logfile_buckets_key
+      # 2. value is a set of strings.  each string represents the bucket type and name for a single logfile
+      set_of_logfile_buckets_key = "all_buckets"                # inner key for our set of all logfile bucket type/name
+
+      mdbucket = @client.bucket(metadata_bucket_name)
+      Riak::Crdt::DEFAULT_BUCKET_TYPES[:map] = metadata_bucket_type
+      map = Riak::Crdt::Map.new(mdbucket, metadata_key)
+      map.sets[set_of_logfile_buckets_key].add "#{@bucket_type} #{@bucket_name}"
+    end
     super
   end
 
@@ -43,38 +66,38 @@ class Riak2Output < BufferedOutput
 
   def write(chunk)
     $log.debug " <<<<<===========\n"
-
     records  = []
-    tags = []
     chunk.msgpack_each { |time, tag, record|
       record[@tag_key] = tag
-      tags << tag
       records << record
       $log.debug record
     }
-    put_now(records, tags)
+    put_now(records)
   end
 
   private
 
-  # TODO: add index for some analysis
-  def put_now(records, tags)
+  def put_now(records)
     if not records.empty? then
-      today = Date.today
-      key = "#{today.to_s}-#{UUIDTools::UUID.timestamp_create.to_s}"
-      robj = Riak::RObject.new(@bucket, key)
-      robj.raw_data = records.to_json
-      robj.indexes['year_int'] << today.year
-      robj.indexes['month_bin'] << "#{today.year}-#{"%02d" % today.month}"
-      tags.each do |tag|
-        robj.indexes['tag_bin'] << tag
-      end
-      robj.content_type = 'application/json'
-      robj.store
-      robj
-    end
-  end
+      threads = []
+      records.each { |record|
+        #if you put log statements here, you must take care to NOT forward fluentd's logs to riak.  you will trigger a recursive avalance of riak storage activity.
+        now = DateTime.now.iso8601(9)
+        key = "#{now}-#{UUIDTools::UUID.timestamp_create.to_s}"
+        # $log.debug "#{@bucket_name} #{key} \n"
 
-end
+        # we are doing the somewhat granular storage, instead of chunked, at the record level.  This probably should be a config option.
+        threads << Thread.new {
+          robj = Riak::RObject.new(@bucket, key)
+          robj.content_type = "application/json"
+          raw_data = "{\"msg_s\":\"#{record["msg"]}\",\"time_dt\":\"#{record["time"]}\",\"src_s\":\"fluentd\"}"
+          # $log.debug "raw_data #{raw_data}\n"
+          robj.raw_data = raw_data
+          robj.store(type: @bucket_type)
+        }
+      }
+    end # if
+  end # put_now
+end # class Riak2Output
 
-end
+end # Module
